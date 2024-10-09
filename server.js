fc647d05e7dfeab8ead38f7733d395dc692d65b5
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-10-09 16:57:40 +0200

    feat/successfully testing and running

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const productRoutes = require('./routes/productRoutes')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
